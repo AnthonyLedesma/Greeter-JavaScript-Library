718034063f0b5170957a84f6592416f96cfffec9
paranoia1906 <anthonymledesma@gmail.com>
2018-06-20 07:26:10 -0700

    Greeter framework/library

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ console.log(g);
 
 g.greet()
 g.greet().greet(true);
-g.greet().setLang('es').greet(true);
+g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function() {
+    let loginGrtr = G$('Anthony', 'Ledesma');
+    $('#logindiv').hide();
+    loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+
+})
--- greeter.js
+++ greeter.js
@@ -7,44 +7,49 @@
 // Support jQuery
 // Easy to type 'G$()' structure.
 
-(function(global, $) {
+;(function(global, $) {
+    // 'new' an object
     var Greeter = function(firstName, lastName, language) {
         return new Greeter.init(firstName, lastName, language);
     }
-
+    // hidden within the scope of the IIFE and never directly accessible.
     var supportedLangs = ['en','es'];
-
+    // Informal greetings
     var greetings = {
         en: "Hello",
         es: "Hola"
     };
-
+    // Formal greetings
     var logMessages = {
         en: "Logged in",
         es: "Inicio Sesion"
     };
-
+    // logger messages
     var formalGreetings = {
         en: "Greetings",
         es: "Saludos"
     };
-
+    // prototype golds methods (to save memory space)
     Greeter.prototype = {
-
+        // 'this' refers to the calling object at execution time
         fullName: function() {
             return this.firstName + ' ' + this.lastName;
         },
         validate: function() {
+            // check that is a valid language
+            // references the externally inaccessible 'supportedLangs' whithin the closure
             if (supportedLangs.indexOf(this.language) === -1) {
                 throw "Invalid Language";
             }
         },
+        // Retriece  messages from object by referring to properties using [] syntax
         greeting: function() {
             return greetings[this.language] + ' ' + this.firstName + '!';
         },
         formalGreeting: function() {
             return formalGreetings[this.language] + ' ' + this.fullName();
         },
+        // Chainable methods return their own containing object
         greet: function(formal) {
             var msg;
 
@@ -58,23 +63,50 @@
             if(console) {
                 console.log(msg);
             }
-
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
             return this;
         },
         log: function() {
             if (console) {
                 console.log(logMessages[this.language] + ': ' + this.fullName());
             }
+            // Make chainable
             return this;
         },
         
         setLang: function(lang) {
+            // Set the language
             this.language = lang;
+            // Validate
             this.validate();
+            // Make chainable
+            return this;
+        },
+
+        HTMLGreeting: function(selector, formal) {
+            if (!$) {
+                throw 'jQuery not loaded';
+            }
+
+            if (!selector) {
+                throw 'Missing jQuery selector';
+            }
+            // Determine the message
+            let msg;
+            if (formal) {
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting();
+            }
+            // Inject the message in the chosen place in the DOM
+            $(selector).html(msg);
+            // Make chainable
             return this;
         }
 
     };
+    // The actual object is created here, allowing us to 'new' an object without calling 'new'
 
     Greeter.init = function(firstName, lastName, language) {
 
@@ -83,10 +115,11 @@
         self.lastName = lastName || '';
         self.language = language || 'en';
 
+        self.validate();
     }   
-
+    // Trick borrowed from jQuery so we don't have to use the 'new' keyword
     Greeter.init.prototype = Greeter.prototype;
-
+    // Attach out greeter to the global object, and provide a shorthand '$G' for ease of our poor fingers
     global.Greeter = global.G$ = Greeter;
 
 }(window, jQuery));
